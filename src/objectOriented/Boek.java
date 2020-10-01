9bcd1730cda127419331b5e7b518574c2a899163
abackiel <aimee.backiel@hotmail.com>
2020-10-01 12:15:31 +0200

    Update Boek.java

--- src/objectOriented/Boek.java
+++ src/objectOriented/Boek.java
@@ -1,34 +1,24 @@
 package objectOriented;
 
 public class Boek {
-    //instance variables
-    String titel;
-    String auteur;
-    int aantalPaginas;
-    int aantalHoofdStukken;
+    //declare your instance variables here
 
-    //constructor: create a Boek object with the given parameters
-    public Boek(String pTitel, String pAuteur, int paginas, int hoofdstukken){
-        titel = pTitel;
-        auteur = pAuteur;
-        aantalPaginas = paginas;
-        aantalHoofdStukken = hoofdstukken;
-    }
+
+    //make a constructor: create a Boek object with the given parameters, set values of the instance variables
+
 
     //create a method that calculates the average number of pages per chapter
-    public int gemiddeldePaginasPerHoofdstuk(){
-        return aantalPaginas/aantalHoofdStukken;
-    }
+    //remember public returnType nameOfMethod()
+
 
     //create a method that creates a String with all the info about each book
-    public String getBoekInfo(){
-        String boekInfo = "Je bent het boek " + titel +
-                " van auteur " + auteur + " aan het lezen,"
-                + " dit is een boek van " + aantalHoofdStukken
-                + " pagina's dik.\nHet gemiddeld aantal pagina's per hoofdstuk zal zijn: " +
-                gemiddeldePaginasPerHoofdstuk();
-        return boekInfo;
-    }
+    //the string that should be output looks like this:
+        /*
+            Je bent het boek HTML&CSS van auteur Duckett aan het lezen, dit is een boek van 999 pagina's dik.
+            Het gemiddeld aantal pagina's per hoofdstuk zal zijn: 99
+         */
+    //Note: you can add a newline inside of the string by typing \n inside the ""
+    //e.g. String multiline = "This is a string that falls\n onto a new line."
 
 
 }
