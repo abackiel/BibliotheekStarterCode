ab4c45fb7e744a1757efb310ebadb970e60a8fc2
abackiel <aimee.backiel@hotmail.com>
2020-10-01 12:16:10 +0200

    Update NewBibliotheekApp.java

--- src/objectOriented/NewBibliotheekApp.java
+++ src/objectOriented/NewBibliotheekApp.java
@@ -1,14 +1,12 @@
 package objectOriented;
 
 public class NewBibliotheekApp {
+   //rewrite your main method here to use the Boek class you've written
     public static void main(String[] args) {
 
         //create two Boek objects
-        Boek html = new Boek("HTML&CSS", "Duckett", 999, 10);
-        Boek javascript = new Boek("JavaScript&jQuery", "Duckett", 1400, 12);
 
         //use System.out.println to print out the getBoekInfo() String for each Boek object
-        System.out.println(html.getBoekInfo());
-        System.out.println(javascript.getBoekInfo());
+
     }
 }
